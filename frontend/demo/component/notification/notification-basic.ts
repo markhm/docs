fa4bb855289f71fc9462ad1d8e4fc6ae235125e1
Tomi Virkki <virkki@vaadin.com>
2020-12-07 19:20:52 +0200

    Use a separate bound renderer

--- frontend/demo/component/notification/notification-basic.ts
+++ frontend/demo/component/notification/notification-basic.ts
@@ -4,48 +4,42 @@ import { render } from 'lit-html';
 import { html, LitElement, customElement, internalProperty } from 'lit-element';
 import '@vaadin/vaadin-button/vaadin-button';
 import '@vaadin/vaadin-lumo-styles/icons';
-import { NotificationElement } from '@vaadin/vaadin-notification/vaadin-notification';
 
 @customElement('notification-basic')
 export class Example extends LitElement {
   @internalProperty()
-  private notification: NotificationElement | null | undefined;
+  private notificationOpen = false;
 
   render() {
     return html`
-      <vaadin-button @click="${this.open}">Try it</vaadin-button>
+      <vaadin-button @click="${() => (this.notificationOpen = true)}">Try it</vaadin-button>
 
       <!-- tag::snippet[] -->
       <vaadin-notification
-        .renderer="${(root: HTMLElement) =>
-          render(
-            html`
-              <div>Financial report generated</div>
-              <vaadin-button
-                theme="tertiary-inline"
-                @click="${this.close.bind(this)}"
-                aria-label="Close"
-              >
-                <iron-icon icon="lumo:cross"></iron-icon>
-              </vaadin-button>
-            `,
-            root
-          )}"
+        .opened=${this.notificationOpen}
+        @opened-changed=${(e: CustomEvent) => (this.notificationOpen = e.detail.value)}
+        .renderer="${this.boundRenderer}"
         position="middle"
       ></vaadin-notification>
       <!-- end::snippet[] -->
     `;
   }
 
-  firstUpdated() {
-    this.notification = this.shadowRoot?.querySelector('vaadin-notification');
-  }
+  private boundRenderer = this.renderer.bind(this);
 
-  open() {
-    this.notification?.open();
-  }
-
-  close() {
-    this.notification?.close();
+  renderer(root: HTMLElement) {
+    render(
+      html`
+        <div>Financial report generated</div>
+        <vaadin-button
+          theme="tertiary-inline"
+          @click="${() => (this.notificationOpen = false)}"
+          aria-label="Close"
+        >
+          <iron-icon icon="lumo:cross"></iron-icon>
+        </vaadin-button>
+      `,
+      root
+    );
   }
 }
